b52c38d02694c40014136a8321dbf1b13e5c01e0
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-02-04 13:56:19 +0300

    Update UIView extensions

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "AutoLayoutHelper",
             dependencies: [],
-            path: "Sources/AutoLayoutHelper"
+            path: "Sources"
         ),
         .testTarget(
             name: "AutoLayoutHelperTests",
--- Sources/AutoLayoutHelper/UIView+Extensions.swift
+++ Sources/AutoLayoutHelper/UIView+Extensions.swift
@@ -1,4 +1,95 @@
-import UIKit.UIView
+import Foundation
+import UIKit
+
+//MARK: - Frame
+public struct UIView {
+    
+    var origin: CGPoint {
+        get {
+            return self.frame.origin
+        } set {
+            self.frame = CGRect(origin: newValue, size: frame.size)
+        }
+    }
+    
+    var size: CGSize {
+        get {
+            return self.frame.size
+        } set {
+            self.frame = CGRect(origin: frame.origin, size: newValue)
+        }
+    }
+    
+    var x: CGFloat {
+        get {
+            return self.frame.origin.x
+        } set {
+            self.frame = CGRect(x: newValue, y: y, width: width, height: height)
+        }
+    }
+    
+    var y: CGFloat {
+        get {
+            return frame.origin.y
+        } set {
+            frame = CGRect(x: x, y: newValue, width: width, height: height)
+        }
+    }
+    
+    var width: CGFloat {
+        get {
+            return self.frame.size.width
+        } set {
+            self.frame = CGRect(x: x, y: y, width: newValue, height: height)
+        }
+    }
+    
+    var height: CGFloat {
+        get {
+            return self.frame.size.height
+        } set {
+            self.frame = CGRect(x: x, y: y, width: width, height: newValue)
+        }
+    }
+    
+    var left: CGFloat {
+        get {
+            return x
+        } set {
+            x = newValue
+        }
+    }
+    
+    var right: CGFloat {
+        get {
+            return x + width
+        } set {
+            x = newValue - width
+        }
+    }
+    
+    var top: CGFloat {
+        get {
+            return y
+        } set {
+           y = newValue
+        }
+    }
+    
+    var bottom: CGFloat {
+        get {
+            return y + height
+        } set {
+            y = newValue - height
+        }
+    }
+}
+
+//MARK: - Anchor
+public struct ViewAnchorWithConstant<T: NSObject> {
+    var anchor: NSLayoutAnchor<T>
+    var constant: CGFloat = 0.0
+}
 
 public extension UIView {
     
@@ -8,68 +99,43 @@ public extension UIView {
                 trailing: NSLayoutXAxisAnchor? = nil,
                 padding: UIEdgeInsets = .zero,
                 size: CGSize = .zero) {
-        translatesAutoresizingMaskIntoConstraints = false
+        self.translatesAutoresizingMaskIntoConstraints = false
         
         if let top = top {
-            topAnchor
+            self.topAnchor
                 .constraint(equalTo: top, constant: padding.top)
                 .isActive = true
         }
         
         if let leading = leading {
-            leadingAnchor
+            self.leadingAnchor
                 .constraint(equalTo: leading, constant: padding.left)
                 .isActive = true
         }
         
         if let bottom = bottom {
-            bottomAnchor
+            self.bottomAnchor
                 .constraint(equalTo: bottom, constant: -padding.bottom)
                 .isActive = true
         }
         
         if let trailing = trailing {
-            trailingAnchor
+            self.trailingAnchor
                 .constraint(equalTo: trailing, constant: -padding.right)
                 .isActive = true
         }
         
         if size.width != 0 {
-            widthAnchor
+            self.widthAnchor
                 .constraint(equalToConstant: size.width)
                 .isActive = true
         }
         
         if size.height != 0 {
-            heightAnchor
+            self.heightAnchor
                 .constraint(equalToConstant: size.height)
                 .isActive = true
         }
-    }
-    
-    func fillSuperView() {
-        anchor(top: superview?.topAnchor,
-               leading: superview?.leadingAnchor,
-               bottom: superview?.bottomAnchor,
-               trailing: superview?.trailingAnchor)
-    }
-    
-    func fillSuperView(with padding: ViewCornerPadding) {
-        let padding = UIEdgeInsets.padding(padding)
-        anchor(top: superview?.topAnchor,
-               leading: superview?.leadingAnchor,
-               bottom: superview?.bottomAnchor,
-               trailing: superview?.trailingAnchor,
-               padding: padding)
-    }
-    
-    func fillSuperView(with equalPadding: CGFloat) {
-        let equalPadding = UIEdgeInsets.equalPadding(equalPadding)
-        anchor(top: superview?.topAnchor,
-               leading: superview?.leadingAnchor,
-               bottom: superview?.bottomAnchor,
-               trailing: superview?.trailingAnchor,
-               padding: equalPadding)
     }
     
     func anchor(top: ViewAnchorWithConstant<NSLayoutYAxisAnchor>? = nil,
@@ -77,42 +143,184 @@ public extension UIView {
                 bottom: ViewAnchorWithConstant<NSLayoutYAxisAnchor>? = nil,
                 trailing: ViewAnchorWithConstant<NSLayoutXAxisAnchor>? = nil) {
         
-        translatesAutoresizingMaskIntoConstraints = false
+        self.translatesAutoresizingMaskIntoConstraints = false
         
         if let top = top {
-            topAnchor
+            self.topAnchor
                 .constraint(equalTo: top.anchor, constant: top.constant)
                 .isActive = true
         }
         
         if let leading = leading {
-            leadingAnchor
+            self.leadingAnchor
                 .constraint(equalTo: leading.anchor, constant: leading.constant)
                 .isActive = true
         }
         
         if let bottom = bottom {
-            bottomAnchor
+            self.bottomAnchor
                 .constraint(equalTo: bottom.anchor, constant: -bottom.constant)
                 .isActive = true
         }
         
         if let trailing = trailing {
-            trailingAnchor
+            self.trailingAnchor
                 .constraint(equalTo: trailing.anchor, constant: -trailing.constant)
                 .isActive = true
         }
     }
 }
 
-public struct ViewAnchorWithConstant<T: NSObject> {
-    var anchor: NSLayoutAnchor<T>
-    var constant: CGFloat = 0.0
+//MARK: - Fill SuperView
+public extension UIView {
+    
+    func fillSuperView() {
+        anchor(top: self.superview?.topAnchor,
+               leading: self.superview?.leadingAnchor,
+               bottom: self.superview?.bottomAnchor,
+               trailing: self.superview?.trailingAnchor)
+    }
+    
+    func fillSuperView(with padding: ViewCornerPadding) {
+        let padding = UIEdgeInsets.padding(padding)
+        anchor(top: self.superview?.topAnchor,
+               leading: self.superview?.leadingAnchor,
+               bottom: self.superview?.bottomAnchor,
+               trailing: self.superview?.trailingAnchor,
+               padding: padding)
+    }
+    
+    func fillSuperView(with equalPadding: CGFloat) {
+        let equalPadding = UIEdgeInsets.equalPadding(equalPadding)
+        anchor(top: self.superview?.topAnchor,
+               leading: self.superview?.leadingAnchor,
+               bottom: self.superview?.bottomAnchor,
+               trailing: self.superview?.trailingAnchor,
+               padding: equalPadding)
+    }
 }
 
-//MARK: - Add Subviews
+//MARK: - Center Anchor
 public extension UIView {
-    func addSubviews(views: [UIView]) {
-        views.forEach { self.addSubview($0) }
+    
+    func centerAnchor(centerY: NSLayoutYAxisAnchor? = nil,
+                      centerX: NSLayoutXAxisAnchor? = nil) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let centerY = centerY {
+            self.centerYAnchor
+                .constraint(equalTo: centerY)
+                .isActive = true
+        }
+        
+        if let centerX = centerX {
+            self.centerXAnchor
+                .constraint(equalTo: centerX)
+                .isActive = true
+        }
+    }
+    
+    func centerAnchorWithConstant(centerY: NSLayoutYAxisAnchor? = nil,
+                                  centerX: NSLayoutXAxisAnchor? = nil,
+                                  x: CGFloat = 0,
+                                  y: CGFloat = 0) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let centerY = centerY {
+            self.centerYAnchor
+                .constraint(equalTo: centerY, constant: y)
+                .isActive = true
+        }
+        
+        if let centerX = centerX {
+            self.centerXAnchor
+                .constraint(equalTo: centerX, constant: x)
+                .isActive = true
+        }
+    }
+    
+    func centerYToSuperView() {
+        guard let superView = self.superview else { return }
+        centerAnchor(centerY: superView.centerYAnchor)
+    }
+    
+    func centerXToSuperView() {
+        guard let superView = self.superview else { return }
+        centerAnchor(centerX: superView.centerXAnchor)
+    }
+    
+    func centerToSuperView() {
+        centerXToSuperView()
+        centerYToSuperView()
+    }
+}
+
+//MARK: - Size Anchor
+public extension UIView {
+    
+    func sizeAnchor(width: CGFloat? = nil, height: CGFloat? = nil) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let width = width {
+            self.widthAnchor
+                .constraint(equalToConstant: width)
+                .isActive = true
+        }
+        
+        if let height = height {
+            self.heightAnchor
+                .constraint(equalToConstant: height)
+                .isActive = true
+        }
+    }
+    
+    func equalWidth(to view: UIView, ratio: CGFloat = 1.0) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.widthAnchor
+            .constraint(equalTo: view.widthAnchor, multiplier: ratio)
+            .isActive = true
+    }
+    
+    func equalHeight(to view: UIView, ratio: CGFloat = 1.0) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.heightAnchor
+            .constraint(equalTo: view.heightAnchor, multiplier: ratio)
+            .isActive = true
+    }
+    
+    func equalSize(to view: UIView, ratio: CGFloat = 1.0) {
+        equalWidth(to: view, ratio: ratio)
+        equalHeight(to: view, ratio: ratio)
+    }
+    
+    func viewAspect(ratio: CGFloat) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.widthAnchor
+            .constraint(equalTo: self.heightAnchor, multiplier: ratio)
+            .isActive = true
+    }
+}
+
+//MARK: - SafeArea Anchor
+public extension UIView {
+    
+    var safeAreaTopAnchor: NSLayoutYAxisAnchor {
+        return self.safeAreaLayoutGuide.topAnchor
+    }
+    
+    var safeAreaLeftAnchor: NSLayoutXAxisAnchor {
+        return self.safeAreaLayoutGuide.leadingAnchor
+    }
+    
+    var safeAreaBottomAnchor: NSLayoutYAxisAnchor {
+        return self.safeAreaLayoutGuide.bottomAnchor
+    }
+    
+    var safeAreaRightAnchor: NSLayoutXAxisAnchor {
+        return self.safeAreaLayoutGuide.trailingAnchor
     }
 }
